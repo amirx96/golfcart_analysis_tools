a26c18edc9dd7ea31e10ee32ed7bf838df778ce3
praditya <adityapratap1.rajawat@gmail.com>
2018-06-26 16:43:38 -0500

    added parsing script for system identificaiton

--- loading_scripts/load_pacmod_parsed_tx_vehicle_speed_rpt.m
+++ loading_scripts/load_pacmod_parsed_tx_vehicle_speed_rpt.m
@@ -3,7 +3,7 @@ data = finddata(S,'_pacmod_parsed_tx_vehicle_speed_rpt');
 if(~isempty(fieldnames(data)))
     [times idx] = unique(data.(1),'stable');
     dat = data.(8);
-    pacmod_spd = timeseries(dat(idx),epoch2mat(times),'Name','Pacmod Speed (mph)'); % 0.44704 is conversion fro mph to m/s
+    pacmod_spd = timeseries(0.8787.*dat(idx),epoch2mat(times),'Name','Pacmod Speed (mph)'); %0.8787 is manual conversion from PACMOD since PACMOD speed is wrong
 %         pacmod_spd = timeseries(dat(idx),times,'Name','Pacmod Speed (m/s)');
 % Not sure what above code does. Why do we want to remove repeat times -
 % Amir
